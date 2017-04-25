ae463b97e1571624a8ce8357838d178aa5a1d3da
vitorpre <vitor.presutti@gmail.com>
2017-04-25 00:35:23 -0300

    - inicio do repositório

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+
+function selecionarAlternativa(elementAlernativaSelecionada)
+{
+	var alternativaSelecionada = $(elementAlernativaSelecionada).data("alternativa");
+
+	$("#alternativaSelecionada").val(alternativaSelecionada);
+
+	estilizarAlternativaSelecionada(elementAlernativaSelecionada);
+}
+
+function estilizarAlternativaSelecionada(elementAlernativaSelecionada)
+{
+	$(".resposta").removeClass("resposta-selecionada");
+
+	$(elementAlernativaSelecionada).addClass("resposta-selecionada");
+}
+
+function validarAlternativaSelecionada()
+{
+	var retorno = true;
+
+	var alternativaSelecionada = $("#alternativaSelecionada").val();
+
+	if ($.inArray( alternativaSelecionada, [ "A", "B", "C", "D", "E" ] ) < 0)
+	{
+		retorno = false;
+	}
+
+	return retorno;
+
+}
+
+$(document).ready(function(){
+
+
+	$(".resposta").on("click", function(){
+
+		selecionarAlternativa(this);
+
+	});
+
+	$("#botaoResponder").on("click", function(){
+
+		
+
+		
+		if(!validarAlternativaSelecionada())
+		{
+			alert("Selecione uma alternativa");
+			return;
+
+		}
+
+		$('#modalConfirmacao').modal('show');
+
+	});
+
+	$("#enviarFormulario").on("click", function(){
+
+		if(!validarAlternativaSelecionada())
+		{
+			alert("Selecione uma alternativa");
+			return;
+
+		}
+
+		$("#enviarResposta").submit();
+
+	});
+
+	
+
+
+});
